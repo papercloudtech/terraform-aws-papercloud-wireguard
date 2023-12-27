53c87ebfa09902b90d17e14a03bc7c78144ebc3f
Sakthi Santhosh <sakthisanthosh010303+github@gmail.com>
2023-12-27 15:08:32 +0530

    fix security flaw

--- user-data.sh
+++ user-data.sh
@@ -11,7 +11,7 @@ apt install wireguard -y
 
 private_key=$(wg genkey)
 
-echo "$private_key" >> sudo tee /etc/wireguard/private.key
+echo "$private_key" >> /etc/wireguard/private.key
 chmod go= /etc/wireguard/private.key
 cat /etc/wireguard/private.key | wg pubkey | tee /etc/wireguard/public.key
 
