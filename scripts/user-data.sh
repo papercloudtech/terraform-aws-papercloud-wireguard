eaa7c9c25c3f046f59944b8ec289c638b2a51a5e
Sakthi Santhosh <sakthisanthosh010303+github@gmail.com>
2024-01-11 14:52:43 +0000

    make migrations and remove ufw setup

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -24,21 +24,13 @@ Address = 10.0.0.0/24
 ListenPort = 51820
 SaveConfig = true
 
-PostUp = ufw route allow in on wg0 out on eth0
 PostUp = iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
-
-PreDown = ufw route delete allow in on wg0 out on eth0
 PreDown = iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE"
 
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p
 
 echo "$wireguard_config" > /etc/wireguard/wg0.conf
-
-ufw --force enable
-ufw allow 51820/udp && ufw allow 22/tcp
-ufw allow 80/tcp && ufw allow 443/tcp
-ufw status
 
 systemctl enable wg-quick@wg0.service && systemctl start wg-quick@wg0.service
 systemctl status wg-quick@wg0.service
@@ -48,6 +40,8 @@ cd /server/
 
 virtualenv -q ./venv/ && source ./venv/bin/activate
 pip3 install -r ./requirements.txt
+
+python3 ./manage.py makemigrations api
 python3 ./manage.py migrate
 DJANGO_SUPERUSER_PASSWORD=ec2-user@12345 python3 ./manage.py createsuperuser --noinput --username=admin --email=admin@openkart.com
 python3 ./manage.py runserver 0.0.0.0:80 > /var/log/server.log 2>&1 &
